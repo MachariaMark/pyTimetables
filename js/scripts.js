d66be8bfc642454805507192c104f52ea1a55b58
danielevans-999 <danielevans.karani@gmail.comgit>
2019-09-04 11:59:48 +0300

    Adds a variable that store the subject name in the busines logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
 // busiiness logic
 $(document).ready(function(){
 
-    function Teacher(name,subject){
-        this.name=name;
-        this.subject=subject;
-    }
+    // function Teacher(name,subject){
+    //     this.name=name;
+    //     this.subject=subject;
+    // }
     
     var numberOfTeachers=[];
     var teacherNames=[];
+    var numberOfSubjects=[];
+    var subjectName=[];
 
 
 
 })
-for(var i=0; i < numberOfTeachers[0]; i++){
-    $("#one").append('<form>'+'<label for="Teacher Name" >Teacher Name</label><input type="text" class="form-control" placeholder="teachername" id="teacher-name" style="margin: 20px;">'+'<br><br>'+'<label for="teacherid">Teacher Id</label><input type="text" class="form-control" placeholder="teacherId" id="teacherid" style="margin: 20px;">'+'</form>')
-    }
+
     
     
 
