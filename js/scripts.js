37d404c8e3b35b6a30e918e74b64a6cfe0b354ad
danielevans-999 <danielevans.karani@gmail.comgit>
2019-09-04 09:01:03 +0300

    Adds constructor in the business logic

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// busiiness logic
+$(document).ready(function(){
+
+    function Teacher(name,subject){
+        this.name=name;
+        this.subject=subject;
+    }
+    
+    var numberOfTeachers=[];
+
+    
+
+
+})
+
+
+
+
