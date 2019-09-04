1cd6099adc370b2589825e91b46eda6199a7ca14
danielevans-999 <danielevans.karani@gmail.comgit>
2019-09-04 11:17:30 +0300

    Adds an Empty variable

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,16 @@ $(document).ready(function(){
     }
     
     var numberOfTeachers=[];
+    var teacherNames=[];
 
-    
 
 
 })
-
+for(var i=0; i < numberOfTeachers[0]; i++){
+    $("#one").append('<form>'+'<label for="Teacher Name" >Teacher Name</label><input type="text" class="form-control" placeholder="teachername" id="teacher-name" style="margin: 20px;">'+'<br><br>'+'<label for="teacherid">Teacher Id</label><input type="text" class="form-control" placeholder="teacherId" id="teacherid" style="margin: 20px;">'+'</form>')
+    }
+    
+    
 
 
 
