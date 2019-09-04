faefbf7045a26984ea16da270894b3fa2d4bbe5e
danielevans-999 <danielevans.karani@gmail.comgit>
2019-09-04 12:52:36 +0300

    Rename the file name in the js folder

--- js/script.js
+++ js/script.js
@@ -10,7 +10,14 @@ $(document).ready(function(){
     var teacherNames=[];
     var numberOfSubjects=[];
     var subjectName=[];
+   
 
+        
+    function ranSubject(){
+        var randomSubject=Math.floor(Math.random()*subject.length)
+        return randomSubject;
+
+    }
 
 
 })
