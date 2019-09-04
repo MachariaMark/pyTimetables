0f4d537acc8c9e2d08ed868f246cf0bf32271b88
Brian Odida <bryodiiidah@gmail.com>
2019-09-04 09:01:01 +0300

    This appends forms to collect teacher data according to the number of teacerhers available

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
 // UI logic
+//UI Logic
+numberOfTeachers.push(5);
+
+for (var i = 0; i < numberOfTeachers[0]; i++) {
+    $("#one").append('<form>' + '<label for="teachersName" >Teacher\'s Name:</label><input type="text" class="form-control" placeholder="Teacher\'s Name" id="teachersName" style="margin: 20px;">' + '<br><br>' + '<label for="teachersId">Teacher\'s Id:</label><input type="text" class="form-control" placeholder="Teacher\'s Id" id="teachersId" style="margin:20px">' + '</form>')
+}
