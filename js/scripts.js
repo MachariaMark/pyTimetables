b3705d54a1ce3c9faf010bfdc7d3f23d033a1740
Brian Odida <bryodiiidah@gmail.com>
2019-09-04 11:15:01 +0300

    This adds  loops to store user data in an empty array

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
-// UI logic
 //UI Logic
-numberOfTeachers.push(5);
 
-for (var i = 0; i < numberOfTeachers[0]; i++) {
-    $("#one").append('<form>' + '<label for="teachersName" >Teacher\'s Name:</label><input type="text" class="form-control" placeholder="Teacher\'s Name" id="teachersName" style="margin: 20px;">' + '<br><br>' + '<label for="teachersId">Teacher\'s Id:</label><input type="text" class="form-control" placeholder="Teacher\'s Id" id="teachersId" style="margin:20px">' + '</form>')
+
+numberOfTeachers.push(8);
+
+for (var i = 1; i <= numberOfTeachers[0]; i++) {
+    $("#one").append('<form class="teacherDetails">' + '<label for="teachersName' + i + '" >Teacher\'s Name:</label><input type="text" class="form-control" placeholder="Teacher\'s Name" id="teachersName' + i + '>' + '<br><br>' + '<label for="teachersId' + i + '">Teacher\'s Id:</label>' + '<input type="text" class="form-control" placeholder="Teacher\'s Id" id="teachersId' + i + '">' + '</form><hr>')
+
+}
+
+
+
+for (var j = 1; j <= numberOfTeachers[0]; j++) {
+    teacherNames.push($("#teachersName" + j).val());
 }
